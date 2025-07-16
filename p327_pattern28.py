644dabecad27138abcc92e3ec64d37dd171b78d3
CodeByNamya <namyapatel30@gmail.com>
2025-07-16 16:14:37 +0530

    Add files via upload

---
+++ p327_pattern28.py
@@ -0,0 +1,7 @@
+n=int(input("enter limit=>"))
+for i in range(n,0,-1):
+    for j in range(1,i+1):
+        print(" ",end="")
+    for j in range(1,i+1):
+        print(i%2,end="")
+    print("")
---
+++ p333_pattern34.py
@@ -0,0 +1,12 @@
+n=int(input("enter limit=>"))
+for i in range(n,0,-1):
+    for j in range(1,i+1):
+        print(" ",end="")
+    for j in range(n,i-1,-1):
+        print("*",end=" ")
+    print("")
+for i in range(1,n):
+    for j in range(1,n+1):
+        print("*",end=" ")
+    print("")
+
